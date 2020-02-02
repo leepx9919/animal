490145dcec8cebf1714f6bce3ee34c879d4b9982
leepx9919 <58961220+leepx9919@users.noreply.github.com>
2020-02-02 00:49:25 -0600

    Initial upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function submit(test) {
+  var x = document.getElementById(test).value;
+
+  if (x % 2 === 0) {
+    document.querySelector("h2").innerHTML = "You are a elephant";
+  } else if (x % 3 === 0) {
+    document.querySelector("h2").innerHTML = "You are a lion";
+  } else if (x % 5 === 0) {
+    document.querySelector("h2").innerHTML = "You are a leopard";
+  } else if (x % 7 === 0) {
+    document.querySelector("h2").innerHTML = "You are a eagle";
+  } else if (x % 1 === 0) {
+    document.querySelector("h2").innerHTML = "You are a sea turtle";
+  } else {
+    document.querySelector("h2").innerHTML = "You are a squirrel for not entering a number.";
+  }
+}
