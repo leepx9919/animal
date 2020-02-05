da51d8479da35a3e0d139fce265d0f0a94b7a309
leepx9919 <58961220+leepx9919@users.noreply.github.com>
2020-02-05 13:29:13 -0600

    Update

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function submit(test) {
   var x = document.getElementById(test).value;
 
   if (x % 2 === 0) {
-    document.querySelector("h2").innerHTML = "You are a elephant";
+    document.querySelector("h2").innerHTML = "You are an elephant";
   } else if (x % 3 === 0) {
     document.querySelector("h2").innerHTML = "You are a lion";
   } else if (x % 5 === 0) {
     document.querySelector("h2").innerHTML = "You are a leopard";
   } else if (x % 7 === 0) {
-    document.querySelector("h2").innerHTML = "You are a eagle";
+    document.querySelector("h2").innerHTML = "You are an eagle";
   } else if (x % 1 === 0) {
     document.querySelector("h2").innerHTML = "You are a sea turtle";
   } else {
